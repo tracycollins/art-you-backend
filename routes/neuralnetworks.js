01b79203906d037883e11dcee69c5d8c25836606
tracycollins <tc@threeceemedia.com>
2021-02-17 22:46:48 -0500

    package

--- routes/neuralnetworks.js
+++ routes/neuralnetworks.js
@@ -1,17 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const NeuralNetworkTools = require("../lib/nnTools.js");
 const nnt = new NeuralNetworkTools("NNT");
 
-nnt.on("ready", () => {
+const statsObj = {};
+statsObj.evolve = {};
+statsObj.evolve.stats = {};
+
+nnt.on("ready", async () => {
   console.log(`NNT READY`)
   nnt.verbose(true)
 })
 
-
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.render('neuralnetworks', { title: 'Neural Networks', visits: req.session.count});
+});
+
+router.get('/test', async function(req, res) {
+  res.render('networkTest', { title: 'Neural Networks Test', visits: req.session.count});
+  await nnt.runNetworkTest();
 });
 
 module.exports = router;
