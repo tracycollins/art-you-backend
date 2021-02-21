b23d5e9fb0b6ec7ad67bb8f0829b7868bb6bc312
tracycollins <tc@threeceemedia.com>
2021-02-21 06:30:28 -0500

    package

--- routes/artworks.js
+++ routes/artworks.js
@@ -29,7 +29,7 @@ router.get('/:id', async (req, res) => {
   console.log(`GET ${model} | ID: ${req.params.id}`)
   query.id = req.params.id
 
-  const doc = await global.artyouDb[model].findOne(query).populate({path: 'artist', populate: { path: 'image'}}).populate('tags').lean();
+  const doc = await global.artyouDb[model].findOne(query).populate('image').populate({path: 'artist', populate: { path: 'image'}}).populate('tags').lean();
   console.log(`FOUND ${model} | ${doc.id} | ${doc.artist.fullName} | ${doc.image.url}`)
 
   res.json(doc)
@@ -39,7 +39,7 @@ router.get('/:id', async (req, res) => {
 router.get('/', async (req, res) => {
   try{
     console.log(`${model} | GET`)
-    const docs = await global.artyouDb[model].find({}).populate({path: 'artist', populate: { path: 'image'}}).populate('tags').lean();
+    const docs = await global.artyouDb[model].find({}).populate('image').populate({path: 'artist', populate: { path: 'image'}}).populate('tags').lean();
     console.log(`FOUND ${docs.length} ${model}s`)
     res.json(docs)
   }
