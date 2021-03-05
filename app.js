8975f601aa8d8311edc6bcbd9e8492ddac8396e6
tracycollins <tc@threeceemedia.com>
2021-03-05 13:20:36 -0500

    package

--- app.js
+++ app.js
@@ -174,6 +174,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(count);
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require("express-rate-limit");
+var limiter = new RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.post("/authenticated", async (req, res) => {
   console.info(`POST /authenticated`, req.body);
   if (req.body && req.body.sub) {
