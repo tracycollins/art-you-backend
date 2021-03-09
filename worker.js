3119f5aed2c44170aa632fbbb4d8fe9514919a5e
tracycollins <tc@threeceemedia.com>
2021-03-09 01:45:36 -0500

    package

--- worker.js
+++ worker.js
@@ -76,13 +76,12 @@ function start() {
   workQueue.process(maxJobsPerWorker, async (job) => {
     try {
       console.log(
-        `${PF} | ->- WORKER | PID: ${process.pid} PROCESS | JOB: ${job}`
+        `${PF} | ->- WORKER | PID: ${process.pid} PROCESS | JOB: OP: ${job.op} ${job.data}`
       );
       const results = await updateUserRecommendations(job);
       console.log(
-        `${PF} | +++ WORKER | PID: ${process.pid} PROCESS | JOB COMPLETE: ${job}`
+        `${PF} | +++ WORKER | PID: ${process.pid} PROCESS | JOB COMPLETE: OP: ${job.op} ${job.data}`
       );
-      console.log({ job });
       return { op: job.op, results: results, stats: statsObj };
     } catch (err) {
       console.log(
