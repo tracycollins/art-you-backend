2eda437a4b09f27df6f6ed938ca6ed21ffcacfeb
tracycollins <tc@threeceemedia.com>
2021-03-12 20:43:03 -0500

    package

--- worker.js
+++ worker.js
@@ -15,13 +15,17 @@ const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 
 // Spin up multiple processes to handle jobs to take advantage of more CPU cores
 // See: https://devcenter.heroku.com/articles/node-concurrency for more info
-const workers = process.env.WORKER_CONCURRENCY || 1;
+const WORKERS = process.env.WORKER_CONCURRENCY || 1;
 
 // The maximum number of jobs each worker should process at once. This will need
 // to be tuned for your application. If each job is mostly waiting on network
 // responses it can be much higher. If each job is CPU-intensive, it might need
 // to be much lower.
 const maxJobsPerWorker = process.env.WORKER_MAX_JOBS || 1;
+
+process.on("SIGTERM", () => {
+  console.log(`${PF} | SIGTERM`);
+});
 
 const NeuralNetworkTools = require("./lib/nnTools.js");
 const nnt = new NeuralNetworkTools(`${PF}_NNT`);
@@ -123,4 +127,8 @@ function start() {
 
 // Initialize the clustered worker process
 // See: https://devcenter.heroku.com/articles/node-concurrency for more info
-throng({ workers, start });
+throng({
+  count: WORKERS,
+  worker: start,
+  grace: 1000,
+});
