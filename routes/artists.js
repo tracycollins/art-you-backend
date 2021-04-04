79d8f1721de0c45465f8c820b93cc4b831a2c20f
tracycollins <tc@threeceemedia.com>
2021-04-04 14:53:26 -0400

    package

--- routes/artists.js
+++ routes/artists.js
@@ -28,6 +28,11 @@ router.get("/cursor/:cursor", async (req, res) => {
 
     console.log(`ARTISTS | GET | ${artists.length} ARTISTS | LIMIT: ${limit}`);
 
+    if (artists.length > 0) {
+      const artist = artists[0];
+      console.log({ artist });
+    }
+
     res.json({ artists, nextKey });
   } catch (err) {
     const message = `GET | ARTISTS | ID: ${req.body.id} | USER ID: ${req.params.userid} | CURSOR: ${req.params.cursor} | ERROR: ${err}`;
@@ -36,208 +41,194 @@ router.get("/cursor/:cursor", async (req, res) => {
   }
 });
 
-// // if subdoc === 'unrated' => sort and value can be null and can be ignored
-// router.get(
-//   "/user/:userid/cursor/:cursorid/(:subdoc)?(.:sort.:value)?",
-//   async (req, res) => {
-//     try {
-//       const userDoc = req.params.userid
-//         ? await global.artyouDb.User.findOne({
-//             id: req.params.userid,
-//           }).select("_id")
-//         : false;
+// if subdoc === 'unrated' => sort and value can be null and can be ignored
+router.get(
+  "/user/:userid/cursor/:cursorid/(:subdoc)?(.:sort.:value)?",
+  async (req, res) => {
+    try {
+      const userDoc = req.params.userid
+        ? await global.artyouDb.User.findOne({
+            id: req.params.userid,
+          }).select("_id")
+        : false;
 
-//       const user_id = userDoc ? userDoc._id.toString() : false;
+      const user_id = userDoc ? userDoc._id.toString() : false;
 
-//       // let userid = req.params.userid || 0;
-//       const cursorid = req.params.cursorid;
-//       const subDoc = req.params.subdoc || "none"; // rating, recommendation, unrated
-//       const sort = req.params.sort || "none"; // name of field :'rate', 'score'
-//       let match = sort && sort !== "none" ? { "user._id": user_id } : {};
-//       const value = req.params.value || false; // field value: rate, score
-//       const limit = process.env.PAGE_SIZE || 20;
+      // let userid = req.params.userid || 0;
+      const cursorid = req.params.cursorid;
+      const subDoc = req.params.subdoc || "none"; // rating, recommendation, unrated
+      const sort = req.params.sort || "none"; // name of field :'rate', 'score'
+      let match = sort && sort !== "none" ? { "user._id": user_id } : {};
+      const value = req.params.value || false; // field value: rate, score
+      const limit = process.env.PAGE_SIZE || 20;
 
-//       const cursor = {};
-//       cursor._id = cursorid;
+      const cursor = {};
+      cursor._id = cursorid;
 
-//       // if (sort) {
-//       if (sort && sort !== "none") {
-//         cursor[sort] = parseInt(value);
-//       }
+      // if (sort) {
+      if (sort && sort !== "none") {
+        cursor[sort] = parseInt(value);
+      }
 
-//       if (subDoc === "unrated") {
-//         if (user_id !== 0) {
-//           console.log(`GET | FOUND USER | _ID: ${user_id}`);
-//           match = {
-//             // "ratings.user._id": { $nin: [user_id] },
-//             // "ratings.user._id": { $nin: [ObjectID(user_id), user_id] },
-//           };
-//           // console.log(`match.ratings.user._id: ${match.ratings.user._id}`);
-//         }
-//       }
+      if (subDoc === "unrated") {
+        if (user_id !== 0) {
+          console.log(`GET | FOUND USER | _ID: ${user_id}`);
+          match = {
+            // "ratings.user._id": { $nin: [user_id] },
+            // "ratings.user._id": { $nin: [ObjectID(user_id), user_id] },
+          };
+          // console.log(`match.ratings.user._id: ${match.ratings.user._id}`);
+        }
+      }
 
-//       console.log(
-//         `GET Artist | URL: ${req.url} | CURSOR | SORT (subDoc): ${subDoc}` +
-//           ` | FILTER BY USER _ID: ${user_id}` +
-//           ` | CURSOR: _id: ${cursor._id} sort: ${sort} value: ${value}`
-//       );
+      console.log(
+        `GET Artist | URL: ${req.url} | CURSOR | SORT (subDoc): ${subDoc}` +
+          ` | FILTER BY USER _ID: ${user_id}` +
+          ` | CURSOR: _id: ${cursor._id} sort: ${sort} value: ${value}`
+      );
 
-//       const paginationOptions = {};
-//       paginationOptions.query = match;
-//       // if (sort) {
-//       if (sort && sort !== "none") {
-//         paginationOptions.sort = [sort, -1];
-//       }
+      const paginationOptions = {};
+      paginationOptions.query = match;
+      // if (sort) {
+      if (sort && sort !== "none") {
+        paginationOptions.sort = [sort, -1];
+      }
 
-//       if (cursor !== undefined && cursor._id !== "0") {
-//         paginationOptions.nextKey = {};
-//         paginationOptions.nextKey._id = cursor._id;
-//         // if (sort) {
-//         if (sort && sort !== "none") {
-//           paginationOptions.nextKey[sort] = cursor[sort];
-//           paginationOptions.sort = [sort, -1];
-//         }
-//       }
+      if (cursor !== undefined && cursor._id !== "0") {
+        paginationOptions.nextKey = {};
+        paginationOptions.nextKey._id = cursor._id;
+        // if (sort) {
+        if (sort && sort !== "none") {
+          paginationOptions.nextKey[sort] = cursor[sort];
+          paginationOptions.sort = [sort, -1];
+        }
+      }
 
-//       console.log({ paginationOptions });
+      console.log({ paginationOptions });
 
-//       const paginationResults = global.artyouDb.generatePaginationQuery(
-//         paginationOptions
-//       );
+      const paginationResults = global.artyouDb.generatePaginationQuery(
+        paginationOptions
+      );
 
-//       const sortByOptions = {};
-//       sortByOptions.user_id = user_id ? ObjectID(user_id) : null;
-//       console.log(
-//         `USER ID ${sortByOptions.user_id} | IS ObjectID: ${ObjectID.isValid(
-//           sortByOptions.user_id
-//         )}`
-//       );
-//       // sortByOptions.user_id = user_id;
-//       sortByOptions.match = paginationResults.paginatedQuery;
+      const sortByOptions = {};
+      sortByOptions.user_id = user_id ? ObjectID(user_id) : null;
+      console.log(
+        `USER ID ${sortByOptions.user_id} | IS ObjectID: ${ObjectID.isValid(
+          sortByOptions.user_id
+        )}`
+      );
+      // sortByOptions.user_id = user_id;
+      sortByOptions.match = paginationResults.paginatedQuery;
 
-//       sortByOptions.limit = limit;
-//       sortByOptions.subDoc = subDoc || "none";
-//       sortByOptions.sort = sort && sort !== "none" ? { [sort]: -1 } : "none";
+      sortByOptions.limit = limit;
+      sortByOptions.subDoc = subDoc || "none";
+      sortByOptions.sort = sort && sort !== "none" ? { [sort]: -1 } : "none";
 
-//       // docs can be ratings or recommendations
-//       const docs = await global.artyouDb.sortBySubDocUserPaginate(
-//         sortByOptions
-//       );
+      // docs can be ratings or recommendations
+      const docs = await global.artyouDb.sortBySubDocUserPaginate(
+        sortByOptions
+      );
 
-//       const nextKey = paginationResults.nextKeyFn(docs);
+      const nextKey = paginationResults.nextKeyFn(docs);
 
-//       if (nextKey) {
-//         console.log({ nextKey });
-//         console.log(
-//           // eslint-disable-next-line no-underscore-dangle
-//           `FOUND Artist BY USER _ID: ${user_id}` +
-//             ` | SUBDOC: ${subDoc}` +
-//             ` | NEXT ID: ${nextKey._id}` +
-//             ` | NEXT SORT: ${nextKey.sort} ` +
-//             ` | NEXT RATE: ${nextKey.rate} ` +
-//             ` | NEXT SCORE: ${nextKey.score}` +
-//             ` | TOP ${docs.length} DOCs`
-//         );
-//       } else {
-//         console.log(
-//           // eslint-disable-next-line no-underscore-dangle
-//           `FOUND Artist BY USER _ID: ${user_id} | SUBDOC: ${subDoc} | NEXT KEY: ${nextKey} | TOP ${docs.length} DOCs`
-//         );
-//       }
+      if (nextKey) {
+        console.log({ nextKey });
+        console.log(
+          // eslint-disable-next-line no-underscore-dangle
+          `FOUND Artist BY USER _ID: ${user_id}` +
+            ` | SUBDOC: ${subDoc}` +
+            ` | NEXT ID: ${nextKey._id}` +
+            ` | NEXT SORT: ${nextKey.sort} ` +
+            ` | NEXT RATE: ${nextKey.rate} ` +
+            ` | NEXT SCORE: ${nextKey.score}` +
+            ` | TOP ${docs.length} DOCs`
+        );
+      } else {
+        console.log(
+          // eslint-disable-next-line no-underscore-dangle
+          `FOUND Artist BY USER _ID: ${user_id} | SUBDOC: ${subDoc} | NEXT KEY: ${nextKey} | TOP ${docs.length} DOCs`
+        );
+      }
 
-//       let artists = [];
+      let artists = [];
 
-//       if (subDoc !== "none" && subDoc && subDoc !== "unrated") {
-//         artists = docs.map((doc) => {
-//           const art = Object.assign({}, doc.artist);
-//           art.ratings = art.ratings || [];
-//           art.recommendations = art.recommendations || [];
-//           art.ratingUser =
-//             subDoc === "rating"
-//               ? doc
-//               : art.ratings.find(
-//                   (rating) =>
-//                     rating.user === user_id || rating.user._id === user_id
-//                 );
-//           art.recommendationUser =
-//             subDoc === "recommendation"
-//               ? doc
-//               : art.recommendations.find(
-//                   (rec) => (rec.user === user_id || rec.user._id) === user_id
-//                 );
-//           return art;
-//         });
-//       } else {
-//         artists = docs.map((artist) => {
-//           console.log({ artist });
-//           // artist.ratingUser = artist.ratings.find(
-//           //   (rating) => rating.user === user_id || rating.user._id === user_id
-//           // );
-//           // artist.recommendationUser = artist.recommendations.find(
-//           //   (rec) => rec.user === user_id || rec.user._id === user_id
-//           // );
-//           return artist;
-//         });
-//       }
+      if (subDoc !== "none" && subDoc && subDoc !== "unrated") {
+        artists = docs.map((doc) => {
+          const art = Object.assign({}, doc.artist);
+          art.ratings = art.ratings || [];
+          art.recommendations = art.recommendations || [];
+          art.ratingUser =
+            subDoc === "rating"
+              ? doc
+              : art.ratings.find(
+                  (rating) =>
+                    rating.user === user_id || rating.user._id === user_id
+                );
+          art.recommendationUser =
+            subDoc === "recommendation"
+              ? doc
+              : art.recommendations.find(
+                  (rec) => (rec.user === user_id || rec.user._id) === user_id
+                );
+          return art;
+        });
+      } else {
+        artists = docs.map((artist) => {
+          console.log({ artist });
+          // artist.ratingUser = artist.ratings.find(
+          //   (rating) => rating.user === user_id || rating.user._id === user_id
+          // );
+          // artist.recommendationUser = artist.recommendations.find(
+          //   (rec) => rec.user === user_id || rec.user._id === user_id
+          // );
+          return artist;
+        });
+      }
 
-//       res.json({ artists: artists, nextKey: nextKey });
-//     } catch (err) {
-//       console.error(
-//         `GET | Artist | OAUTHID: ${req.params.userid} ERROR: ${err}`
-//       );
-//       res
-//         .status(400)
-//         .send(`GET | Artist | OAUTHID: ${req.params.userid} | ERROR: ${err}`);
-//     }
-//   }
-// );
+      res.json({ artists: artists, nextKey: nextKey });
+    } catch (err) {
+      console.error(
+        `GET | Artist | OAUTHID: ${req.params.userid} ERROR: ${err}`
+      );
+      res
+        .status(400)
+        .send(`GET | Artist | OAUTHID: ${req.params.userid} | ERROR: ${err}`);
+    }
+  }
+);
 
-// router.get("/user/:userid/id/:artistId/", async (req, res) => {
-//   try {
-//     const userDoc =
-//       req.params.userid !== "0"
-//         ? await global.artyouDb.User.findOne({
-//             id: req.params.userid,
-//           }).select("_id")
-//         : false;
+router.get("/user/:userid/id/:artistId/", async (req, res) => {
+  try {
+    const userDoc =
+      req.params.userid !== "0"
+        ? await global.artyouDb.User.findOne({
+            id: req.params.userid,
+          }).select("_id")
+        : false;
 
-//     const user_id = userDoc ? userDoc._id.toString() : false;
-//     const artistId = req.params.artistId || false;
+    const user_id = userDoc ? userDoc._id.toString() : false;
+    const artistId = req.params.artistId || false;
 
-//     console.log(
-//       `GET Artist | URL: ${req.url}` +
-//         ` | USER _ID: ${user_id}` +
-//         ` | ARTIST ID: ${artistId}`
-//     );
+    console.log(
+      `GET Artist | URL: ${req.url}` +
+        ` | USER _ID: ${user_id}` +
+        ` | ARTIST ID: ${artistId}`
+    );
 
-//     // docs can be ratings or recommendations
-//     const artist = await global.artyouDb.Artist.findOne({ id: artistId })
-//       .populate("image")
-//       .populate("ratings")
-//       .populate("recommendations")
-//       .lean();
+    const artist = await global.artyouDb.Artist.findOne({ id: artistId })
+      .populate("image")
+      .lean();
 
-//     console.log(
-//       `FOUND ARTIST BY ID } | ID: ${artist.id} | _ID: ${artist._id} | ${
-//         artist.ratings.length
-//       } RATINGS | RATING USER: ${
-//         artist.ratingUser ? artist.ratingUser.user : "none"
-//       }`
-//     );
+    console.log(`FOUND ARTIST BY ID } | ID: ${artist.id} | _ID: ${artist._id}`);
 
-//     for (const rating of artist.ratings) {
-//       console.log(`RATING | ${rating._id} | USER: ${rating.user}`);
-//     }
-//     res.json({ artist: artist });
-//   } catch (err) {
-//     console.error(
-//       `GET | Artist | OAUTHID: ${req.params.userid} ERROR: ${err}`
-//     );
-//     res
-//       .status(400)
-//       .send(`GET | Artist | OAUTHID: ${req.params.userid} | ERROR: ${err}`);
-//   }
-// });
+    res.json({ artist: artist });
+  } catch (err) {
+    console.error(`GET | Artist | OAUTHID: ${req.params.userid} ERROR: ${err}`);
+    res
+      .status(400)
+      .send(`GET | Artist | OAUTHID: ${req.params.userid} | ERROR: ${err}`);
+  }
+});
 
 // router.get("/top-recs/user/:id", async (req, res) => {
 //   try {
